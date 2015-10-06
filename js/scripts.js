32359c045c3334d388a7a748c96c0f1c9f0e6975
John Klaras & Nathan Romike <nathanromike@gmail.com>
2015-10-05 20:00:27 -0700

    got the goshdarned thing to work

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var person1Input = $("input#person1").val();
     var person2Input = $("input#person2").val();
     var animalInput = $("input#animal").val();
-    var exclamation = $("input#exclamtion").val();
+    var exclamationInput = $("input#exclamation").val();
     var verbInput = $("input#verb").val();
     var nounInput = $("input#noun").val();
 
